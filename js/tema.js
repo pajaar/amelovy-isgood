9c40689ac6e1cf92f2eef837cd7b5e30d300ece5
pajaar <53967072+pajaar@users.noreply.github.com>
2023-11-16 15:08:42 -0800

    Crt tema.js

--- js/tema.js
+++ js/tema.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   var params = new URLSearchParams(window.location.search);
-  var rabiCoeg = params.get("no");
+  var rabiCoeg = params.get("s");
   if (rabiCoeg !== null) {
-    document.title = "Contoh tema " + rabiCoeg;
+    document.title = "Contoh tema " + params.get("tema");
     var rabi = "tm-" + rabiCoeg;
     var base = "aHR0cHM6Ly93d3cua2FsYWhhcnUueHl6Lw==";
     var awkey = window.atob(base) + rabi;
